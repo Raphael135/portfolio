f624dfbf63282487019ddde2319dea6ad2a4622f
Raphael Rodolfo <gomesrodolfo656@gmail.com>
2025-08-18 17:22:17 -0300

    Portfolio Finalizado

--- portifolio/script.js
+++ portifolio/script.js
@@ -4,8 +4,9 @@ const modalTitle = document.getElementById("modalTitle");
 const modalDescription = document.getElementById("modalDescription");
 const modalLink = document.getElementById("modalLink");
 const closeModal = document.getElementById("closeModal");
+const btnTopo = document.getElementById("btn-topo");
 
-document.querySelectorAll(".projects").forEach(img => {
+document.querySelectorAll(".projects").forEach((img) => {
   img.addEventListener("click", () => {
     modal.style.display = "flex";
     modalImg.src = img.src;
@@ -19,9 +20,21 @@ closeModal.addEventListener("click", () => {
   modal.style.display = "none";
 });
 
-modal.addEventListener("click", e => {
+modal.addEventListener("click", (e) => {
   if (e.target === modal) {
     modal.style.display = "none";
   }
 });
 
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      btnTopo.style.display = "block";
+    } else {
+      btnTopo.style.display = "none";
+    }
+  });
+
+  btnTopo.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
